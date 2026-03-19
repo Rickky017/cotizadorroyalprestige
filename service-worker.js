6a6aa616a7e55180472f9bcc411c7591e3a55ac0
Rickky017 <luisricardo126@gmail.com>
2026-03-19 16:11:10 -0400

    Refactor service worker caching strategy

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,18 @@
 const CACHE_NAME = "cotizador-pro-v1";
 
-const urlsToCache = [
-  "./",
-  "./index.html"
-];
-
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html"
+      ]);
+    })
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
