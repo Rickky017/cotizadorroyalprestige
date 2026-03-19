0a5a60eb57b1fdb84e871aa715121d3802cad69a
Rickky017 <luisricardo126@gmail.com>
2026-03-19 18:37:27 -0400

    Revise service worker for new cache strategy
    Updated cache name and added new assets for caching.

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,19 @@
-const CACHE_NAME = "cotizador-pro-v1";
+const CACHE_NAME = 'silwey-v1';
+const ASSETS = [
+  'index.html',
+  'https://cdn.tailwindcss.com'
+];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html"
-      ]);
-    })
+// Instalación y cacheo de archivos
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+// Respuesta desde el caché
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
