a265dfe072ea7ad5083335b80c0540afe9a8497f
Rickky017 <luisricardo126@gmail.com>
2026-03-19 20:16:43 -0400

    Revise service worker for caching and fetch strategy
    Updated service worker to include additional assets and changed fetch strategy.

--- sw.js
+++ sw.js
@@ -1,19 +1,21 @@
 const CACHE_NAME = 'silwey-v1';
 const ASSETS = [
-  'index.html',
-  'https://cdn.tailwindcss.com'
+  './',
+  './index.html',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap'
 ];
 
-// Instalación y cacheo de archivos
+// Instalación y cacheo de archivos críticos
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Respuesta desde el caché
+// Estrategia: Primero Red, si falla, Caché
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
